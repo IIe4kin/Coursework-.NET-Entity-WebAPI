2257f34a5bd129be6373b271ea89ba85f70cb0d3
Lalak Bohdan <lalakbo@gmail.com>
2017-05-23 20:17:07 +0300

    Created Script file for creating table in CentralOfficeDB. TODO: finish code

---
+++ CursWorkDB/CursWorkDB/AddTablesScript.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Employee]
+( 
+	[Id] int PRIMARY KEY NOT NULL,
+	[LastName] nvarchar(30) NOT NULL,
+	[FirstName] nvarchar(30) NOT NULL,
+	[DateOfBirth] date NOT NULL,
+	[Post] nvarchar(30) NOT NULL,
+	[Department] nvarchar(MAX) NOT NULL,
+	[Resume] varbinary NULL
+)
+
+CREATE TABLE [dbo].[Post]
+(
+	[Id] int PRIMARY KEY NOT NULL,
+	[Name] nvarchar(50)
+)
+
+/* TODO: Add code to create other tables*/
